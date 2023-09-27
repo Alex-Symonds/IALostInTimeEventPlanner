fd48ed4c4bb3243045a5dab1b7e2de295570f760
Alex Symonds <alex-symonds@gmail.com>
2023-09-27 11:41:04 +0100

    Bug fix: colour of tooltip button in resultsAtTop
    The text on the tooltip button now changes colour to match the
    background colour.

--- app/components/planner/subcomponents/resultAtTop.tsx
+++ app/components/planner/subcomponents/resultAtTop.tsx
@@ -7,6 +7,7 @@ import { calcResultOfPlan } from '../../../utils/calcResults';
 
 import Tooltip from '../../subcomponents/tooltip';
 
+
 interface I_ResultAtTop { 
     planData : T_PurchaseData[], 
     gameState : T_GameState, 
@@ -17,20 +18,20 @@ export default function ResultAtTop({gameState, actions, timeIDGroups}
     : I_ResultAtTop)
     : JSX.Element {
 
-    let resultOfPlan = calcResultOfPlan({gameState, actions, timeIDGroups});
-    let conditionalCSS = 
+    const resultOfPlan = calcResultOfPlan({gameState, actions, timeIDGroups});
+    const bgColourCSS = 
         resultOfPlan.hasWon ?
-            "bg-green-700 text-white"
-            : "bg-red-700 text-white";
+            "bg-green-700"
+            : "bg-red-700";
 
     return(
-        <div className={"sticky z-10 top-12 md:top-[7.5rem] shadow-md py-2 flex flex-col items-center w-full mb-4" + " " + conditionalCSS}>
+        <div className={`sticky z-10 top-12 md:top-[7.5rem] w-full mb-4 py-2 flex flex-col items-center shadow-md text-white ${bgColourCSS}`}>
             <div className={"font-bold text-lg"}>
                 { resultOfPlan.hasWon ? "" : "NOT ENOUGH FOR " }1ST PRIZE
             </div>
             { resultOfPlan.allToDust === null ?
                 <Result dustAtEnd={resultOfPlan.dustAtEnd} />
-                : <ResultWithRec dustAtEnd={resultOfPlan.dustAtEnd} suggestionData={resultOfPlan.allToDust} />
+                : <ResultWithRec dustAtEnd={resultOfPlan.dustAtEnd} suggestionData={resultOfPlan.allToDust} hasWon={resultOfPlan.hasWon} />
             }
         </div>
     )
@@ -47,8 +48,8 @@ function Result({dustAtEnd}
 }
 
 
-function ResultWithRec({dustAtEnd, suggestionData}
-    : { dustAtEnd: number, suggestionData : T_SuggestionData })
+function ResultWithRec({dustAtEnd, suggestionData, hasWon}
+    : { dustAtEnd: number, suggestionData : T_SuggestionData, hasWon : boolean })
     : JSX.Element {
 
     const [showTooltip, setShowTooltip] = useState(false);
@@ -61,7 +62,7 @@ function ResultWithRec({dustAtEnd, suggestionData}
 
     return  <div className={"relative flex flex-col"}>
                 <ResultWithRecRow label={"now"} quantity={dustAtEnd} />
-                <ResultWithRecRow label={"rec."} quantity={suggestionData.dust} suggestionProps={suggestionProps} />
+                <ResultWithRecRow label={"rec."} quantity={suggestionData.dust} suggestionProps={suggestionProps} hasWon={hasWon} />
             </div>
 }
 
@@ -71,9 +72,13 @@ type T_SuggestionProps = {
     setVisibility : Dispatch<SetStateAction<boolean>>,
     position : number 
 }
-function ResultWithRecRow({label, quantity, suggestionProps} 
-    : { label : string, quantity : number, suggestionProps? : T_SuggestionProps}) 
+function ResultWithRecRow({label, quantity, suggestionProps, hasWon} 
+    : { label : string, quantity : number, suggestionProps? : T_SuggestionProps, hasWon? : boolean }) 
     : JSX.Element {
+
+    const buttonTextColour = hasWon ?
+        "text-green-700"
+        : "text-red-700";
 
     return  <div className={"grid grid-rows-1 [grid-template-columns:4rem_minmax(0,_1fr)_3rem_1.75rem]"}>
                 <div className={"font-semibold"}>{ label }</div>
@@ -83,7 +88,7 @@ function ResultWithRecRow({label, quantity, suggestionProps}
                     <div className={"text-right relative"}>
                         <button 
                             onClick={() => suggestionProps.setVisibility(prev => !prev)} 
-                            className={"rounded-full w-5 h-5 bg-white hover:opacity-90 text-red-700 font-bold text-sm"}
+                            className={`rounded-full w-5 h-5 text-center bg-white hover:opacity-90 font-bold text-sm ${buttonTextColour}`}
                             >
                             ?
                         </button>
