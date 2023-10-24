08ec3279ca4bdf2a9082248e33e8edc2f3e3e0aa
Alex Symonds <alex-symonds@gmail.com>
2023-10-24 15:09:23 +0100

    fix: IA:LiT Planner asset URLs
    Added .nojekyll file so the _next folder is served.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'dist'
+    distDir: 'dist',
+    assetPrefix: "https://alex-symonds.github.io/ialitplanner/"
 }
 
 module.exports = nextConfig
